571b5a1d8b19d40cdd3cf96b3196535678ca955d
SandeepPS1299 <sandeep.ps@quadrasystems.net>
2024-06-20 21:07:20 +0530

    chore: clean up default personas and prompts

--- src/features/prompt-page/prompt-hero/prompt-hero.tsx
+++ src/features/prompt-page/prompt-hero/prompt-hero.tsx
@@ -28,51 +28,6 @@ export const PromptHero = (props: PromptHeroProps) => {
             onClick={() => promptStore.newPrompt()}
           />
         )}
-        <HeroButton
-          title="Whimsical City"
-          description="Image of miniature colourful city "
-          icon={<Atom />}
-          onClick={() =>
-            promptStore.updatePrompt({
-              createdAt: new Date(),
-              id: "",
-              name: "Whimsical City",
-              description:
-                "Create A miniature city with colourful buildings and green trees with [iconic building]. The [iconic building] is in the centre of the image, surrounded by a blurred background with lots of [Native tree name] trees. The image has a dreamy and whimsical mood, with a shallow depth of field and a high angle view. The city looks like a toy or a model, with different styles and shapes of buildings.",
-              isPublished: false,
-              type: "PROMPT",
-              userId: "",
-            })
-          }
-        />
-        <HeroButton
-          title="Problem Framing"
-          description="Problem Framing for a new product"
-          icon={<Frame />}
-          onClick={() =>
-            promptStore.updatePrompt({
-              createdAt: new Date(),
-              id: "",
-              name: "Problem Framing",
-              description: `
-Given the following problem statement:
-[PROBLEM STATEMENT]
-
-Generate a response with the following points:
-1. Problem framing
-2. Solution overview and recommendations 
-3. List down the recommended MVP Scope
-4. How to ensure user adoption
-5. How to measure success 
-8. List down similar products
-9. Potential sponsor question (5 questions) 
-              `,
-              isPublished: false,
-              type: "PROMPT",
-              userId: "",
-            })
-          }
-        />
       </Hero>
     </div>
   );
