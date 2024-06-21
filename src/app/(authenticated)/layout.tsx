caa73ddc12c777e5495e355b8b55af0faada30e7
SandeepPS1299 <sandeep.ps@quadrasystems.net>
2024-06-21 12:44:15 +0530

    style: update image opacity and blurness

--- src/app/(authenticated)/layout.tsx
+++ src/app/(authenticated)/layout.tsx
@@ -20,17 +20,21 @@ export default function RootLayout({
       <div className={cn("flex flex-1 items-stretch")}>
         <img
           src={"login_ilustration1.svg"}
-          className="absolute w-1/2 -top-40 -left-32 blur-2xl"
+          className="absolute w-1/2 -top-40 -left-32 blur-3xl opacity-80"
         />
         <img
           src={"login_ilustration1.svg"}
-          className="absolute w-1/3 top-auto -bottom-40 left-36 blur-2xl"
+          className="absolute w-1/3 top-auto -bottom-40 left-36 blur-3xl opacity-80"
         />
         <img
           src={"login_ilustration1.svg"}
           className="absolute top-auto left-1/2 transform -translate-x-1/4 w-2/4"
         />
 
+        <img
+          src={"login_ilustration1.svg"}
+          className="absolute w-1/3 -right-40 -bottom-40  blur-3xl opacity-80"
+        />
         {/* <img src={"login_ilustration1.svg"} className="absolute" /> */}
         <MainMenu />
         <div className="flex-1 flex">{children}</div>
