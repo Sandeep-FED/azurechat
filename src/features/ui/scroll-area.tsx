3510afbbe82499d32e9e967dc2a2f32dd7b6e819
SandeepPS1299 <sandeep.ps@quadrasystems.net>
2024-06-26 19:41:50 +0530

    style: update scrollbar thumb color

--- src/features/ui/scroll-area.tsx
+++ src/features/ui/scroll-area.tsx
@@ -68,7 +68,7 @@ const ScrollBar = React.forwardRef<
     )}
     {...props}
   >
-    <ScrollAreaPrimitive.Thumb className="relative flex-1 rounded-full bg-border" />
+    <ScrollAreaPrimitive.Thumb className="relative flex-1 rounded-full dark:bg-gray-600 bg-gray-400" />
   </ScrollAreaPrimitive.Scrollbar>
 ));
 ScrollBar.displayName = ScrollAreaPrimitive.ScrollAreaScrollbar.displayName;
