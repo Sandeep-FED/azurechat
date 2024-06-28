a9c9634f21abffd2fbb68325f72677321021de71
SandeepPS1299 <sandeep.ps@quadrasystems.net>
2024-06-28 07:48:05 +0530

    fix: tooltip isnt showing for menu items[2/2]

--- src/features/main-menu/menu-tray-toggle.tsx
+++ src/features/main-menu/menu-tray-toggle.tsx
@@ -3,22 +3,34 @@ import { cn } from "@/ui/lib";
 import { MenuItem, menuIconProps } from "@/ui/menu";
 import { PanelLeftClose } from "lucide-react";
 import { menuStore, useMenuState } from "./menu-store";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "../ui/tooltip";
 
 export const MenuTrayToggle = () => {
   const { isMenuOpen } = useMenuState();
 
   return (
-    <MenuItem
-      onClick={() => menuStore.toggleMenu()}
-      tooltip="Open and Collapse menu"
-    >
-      <PanelLeftClose
-        {...menuIconProps}
-        className={cn(
-          "transition-all rotate-180 duration-700",
-          isMenuOpen ? "rotate-0" : ""
-        )}
-      />
-    </MenuItem>
+    <TooltipProvider>
+      <Tooltip>
+        <TooltipTrigger asChild>
+          <MenuItem onClick={() => menuStore.toggleMenu()}>
+            <PanelLeftClose
+              {...menuIconProps}
+              className={cn(
+                "transition-all rotate-180 duration-700",
+                isMenuOpen ? "rotate-0" : ""
+              )}
+            />
+          </MenuItem>
+        </TooltipTrigger>
+        <TooltipContent>
+          <p>Show and Hide Menu</p>
+        </TooltipContent>
+      </Tooltip>
+    </TooltipProvider>
   );
 };
