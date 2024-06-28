cbb731e74680b74ad5b6953bbaac223da3826ed3
SandeepPS1299 <sandeep.ps@quadrasystems.net>
2024-06-28 07:37:37 +0530

    fix: main menu tooltip isnt showing when hovering icons

--- src/features/main-menu/main-menu.tsx
+++ src/features/main-menu/main-menu.tsx
@@ -17,6 +17,12 @@ import {
 import { getCurrentUser } from "../auth-page/helpers";
 import { MenuLink } from "./menu-link";
 import { UserProfile } from "./user-profile";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "../ui/tooltip";
 
 export const MainMenu = async () => {
   const user = await getCurrentUser();
@@ -25,67 +31,125 @@ export const MainMenu = async () => {
     <Menu>
       <MenuBar className="bg-gradient-to-t from-[#2b3d64]/30 to-[#24538D]/30 border-0">
         <MenuItemContainer>
-          {/* <MenuItem tooltip="Home" asChild>
-            <MenuLink href="/chat" ariaLabel="Go to the Home page">
-              <Home {...menuIconProps} className="dark:text-white" />
-            </MenuLink>
-          </MenuItem> */}
           <MenuTrayToggle />
         </MenuItemContainer>
         <MenuItemContainer>
-          <MenuItem tooltip="Chat">
-            <MenuLink href="/chat" ariaLabel="Go to the Chat page">
-              <Home {...menuIconProps} />
-            </MenuLink>
-          </MenuItem>
-          <MenuItem tooltip="Persona">
-            <MenuLink
-              href="/persona"
-              ariaLabel="Go to the Persona configuration page"
-            >
-              <VenetianMask {...menuIconProps} />
-            </MenuLink>
-          </MenuItem>
+          <TooltipProvider>
+            <Tooltip>
+              <TooltipTrigger asChild>
+                <MenuItem>
+                  <MenuLink href="/chat" ariaLabel="Go to the Chat page">
+                    <Home {...menuIconProps} />
+                  </MenuLink>
+                </MenuItem>
+              </TooltipTrigger>
+              <TooltipContent>
+                <p>Home</p>
+              </TooltipContent>
+            </Tooltip>
+          </TooltipProvider>
+          <TooltipProvider>
+            <Tooltip>
+              <TooltipTrigger asChild>
+                <MenuItem>
+                  <MenuLink
+                    href="/persona"
+                    ariaLabel="Go to the Persona configuration page"
+                  >
+                    <VenetianMask {...menuIconProps} />
+                  </MenuLink>
+                </MenuItem>
+              </TooltipTrigger>
+              <TooltipContent>
+                <p>Personas</p>
+              </TooltipContent>
+            </Tooltip>
+          </TooltipProvider>
           {user.isAdmin && (
-            <MenuItem tooltip="Extensions">
-              <MenuLink
-                href="/extensions"
-                ariaLabel="Go to the Extensions configuration page"
-              >
-                <Blocks {...menuIconProps} />
-              </MenuLink>
-            </MenuItem>
+            <TooltipProvider>
+              <Tooltip>
+                <TooltipTrigger asChild>
+                  <MenuItem>
+                    <MenuLink
+                      href="/extensions"
+                      ariaLabel="Go to the Extensions configuration page"
+                    >
+                      <Blocks {...menuIconProps} />
+                    </MenuLink>
+                  </MenuItem>
+                </TooltipTrigger>
+                <TooltipContent>
+                  <p>Extensions</p>
+                </TooltipContent>
+              </Tooltip>
+            </TooltipProvider>
           )}
-          <MenuItem tooltip="Prompts">
-            <MenuLink
-              href="/prompt"
-              ariaLabel="Go to the Prompt Library configuration page"
-            >
-              <BookText {...menuIconProps} />
-            </MenuLink>
-          </MenuItem>
+          <TooltipProvider>
+            <Tooltip>
+              <TooltipTrigger asChild>
+                <MenuItem>
+                  <MenuLink
+                    href="/prompt"
+                    ariaLabel="Go to the Prompt Library configuration page"
+                  >
+                    <BookText {...menuIconProps} />
+                  </MenuLink>
+                </MenuItem>
+              </TooltipTrigger>
+              <TooltipContent>
+                <p>Prompts Library</p>
+              </TooltipContent>
+            </Tooltip>
+          </TooltipProvider>
           {user.isAdmin && (
             <>
-              <MenuItem tooltip="Reporting">
-                <MenuLink
-                  href="/reporting"
-                  ariaLabel="Go to the Admin reporting"
-                >
-                  <Sheet {...menuIconProps} />
-                </MenuLink>
-              </MenuItem>
+              <TooltipProvider>
+                <Tooltip>
+                  <TooltipTrigger asChild>
+                    <MenuItem>
+                      <MenuLink
+                        href="/reporting"
+                        ariaLabel="Go to the Admin reporting"
+                      >
+                        <Sheet {...menuIconProps} />
+                      </MenuLink>
+                    </MenuItem>
+                  </TooltipTrigger>
+                  <TooltipContent>
+                    <p>Reports</p>
+                  </TooltipContent>
+                </Tooltip>
+              </TooltipProvider>
             </>
           )}
         </MenuItemContainer>
         <MenuItemContainer>
-          <MenuItem tooltip="Help">
-            <MenuLink href="/help" ariaLabel="Go to the FAQ page">
-              <HelpCircle {...menuIconProps} />
-            </MenuLink>
-          </MenuItem>
-          <MenuItem tooltip="Profile">
-            <UserProfile />
-          </MenuItem>
+          <TooltipProvider>
+            <Tooltip>
+              <TooltipTrigger asChild>
+                <MenuItem>
+                  <MenuLink href="/help" ariaLabel="Go to the FAQ page">
+                    <HelpCircle {...menuIconProps} />
+                  </MenuLink>
+                </MenuItem>
+              </TooltipTrigger>
+              <TooltipContent>
+                <p>Help</p>
+              </TooltipContent>
+            </Tooltip>
+          </TooltipProvider>
+          <TooltipProvider>
+            <Tooltip>
+              <TooltipTrigger asChild>
+                <MenuItem>
+                  <UserProfile />
+                </MenuItem>
+              </TooltipTrigger>
+              <TooltipContent>
+                <p>Profile</p>
+              </TooltipContent>
+            </Tooltip>
+          </TooltipProvider>
         </MenuItemContainer>
       </MenuBar>
     </Menu>
